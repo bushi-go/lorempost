2dc1ca60ef7ab4cefa804b8302ed89322f45cced
Romain Goussu <rgoussu.develop@gmail.com>
2020-07-16 17:55:04 +0000

    feat: initialized project with Spring Initializr

---
+++ src/main/java/com/maiia/techtest/lorempost/LorempostApplication.java
@@ -0,0 +1,13 @@
+package com.maiia.techtest.lorempost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LorempostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LorempostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maiia/techtest/lorempost/LorempostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiia.techtest.lorempost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LorempostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
